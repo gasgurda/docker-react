192009e8ac22cc48ba40ec8d2ccf68caf93949e9
Timur Eshpulatov <timur.eshpulatov@gmail.com>
2019-03-24 21:43:44 +0300

    Initial commit

---
+++ build/precache-manifest.e133ef3faba55d1b0dd79155c0f2b256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6cfad3168b78ff4d7d05",
+    "url": "/static/js/main.46f4f0b5.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "6cfad3168b78ff4d7d05",
+    "url": "/static/css/main.25de8cc7.chunk.css"
+  },
+  {
+    "revision": "d495174c0ab6d2b597ea0b574a535594",
+    "url": "/index.html"
+  }
+];
